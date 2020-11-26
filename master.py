a08a5f4ab2e3240a078858fc3b0c9bf55ae22d96
Elisa <elisecene@gmail.com>
2020-11-26 22:47:39 +0100

    new structure

---
+++ master.py
@@ -0,0 +1,11 @@
+import os
+
+
+def main():
+    os.system('python src/genes_feature_extraction.py')
+
+    os.system('python src/images_feature_extraction.py')
+
+
+if __name__ == '__main__':
+    main()

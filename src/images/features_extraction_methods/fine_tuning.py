13588e77269a0aab549789857f1f8800b74cebca
=Cristian Traina <crissstian96@gmail.com>
2021-03-31 22:55:21 +0200

    cose

--- src/images/features_extraction_methods/fine_tuning.py
+++ src/images/features_extraction_methods/fine_tuning.py
@@ -16,15 +16,26 @@ import random
 
 def fine_tuning(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir, tumor_selected_tiles_dir):
 
-    train_gen = feed_slides_generator(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir, tumor_selected_tiles_dir, 3, mode="train")
-    eval_gen = feed_slides_generator(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir, tumor_selected_tiles_dir, 3, mode="eval")
-    test_gen = feed_slides_generator(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir, tumor_selected_tiles_dir, 3, mode="eval")
 
-    train_len = len(normal_slides_info) + len(tumor_slides_info) - 5
-    val_len = 5
+    #TODO: move to utils function
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
 
-    model = ResNet50(weights='imagenet', include_top=True)
-    model = Model(inputs=model.inputs, outputs=model.get_layer('avg_pool').output)
+    batch_size = 500
+    train_gen = feed_slides_generator(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir,
+                                      tumor_selected_tiles_dir, batch_size, mode="train")
+    eval_gen = feed_slides_generator(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir,
+                                     tumor_selected_tiles_dir, batch_size, mode="eval")
+    test_gen = feed_slides_generator(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir,
+                                     tumor_selected_tiles_dir, batch_size, mode="eval")
+
+    train_len = get_train_ds_len(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir, tumor_selected_tiles_dir)
+    val_len = get_eval_ds_len(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir, tumor_selected_tiles_dir)
+
+    # model = ResNet50(weights='imagenet', include_top=True)
+    # model = Model(inputs=model.inputs, outputs=model.get_layer('avg_pool').output)
 
     # print(model.summary())
 
@@ -51,34 +62,38 @@ def fine_tuning(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir
     print("[INFO] training head...")
     H = model.fit(
         x=train_gen,
-        steps_per_epoch= train_len // 3,
+        steps_per_epoch=train_len // batch_size,
         validation_data=eval_gen,
-        validation_steps= val_len // 3,
-        epochs=1)
+        validation_steps=val_len // batch_size,
+        epochs=10)
 
-    predIdxs = model.predict(x=test_gen, steps=(val_len // 3) + 1)
+    model.save("fine_tuned")
+
+    predIdxs = model.predict(x=test_gen, steps=(val_len // batch_size) + 1)
     predIdxs = np.argmax(predIdxs, axis=1)
 
     # print(classification_report({}, predIdxs))
-    plot_training(H, 50)
+    plot_training(H, 10)
 
 
-def feed_slides_generator(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir, tumor_selected_tiles_dir, batch_size, mode='train'):
+def feed_slides_generator(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir,
+                          tumor_selected_tiles_dir, batch_size, mode='train'):
     slides_info = [*normal_slides_info, *tumor_slides_info]
     random.shuffle(slides_info)
 
     train_slides = filter(lambda s: s['slide_name'].startswith('b') or s['slide_name'].startswith('f'), slides_info)
     eval_slides = filter(lambda s: not s['slide_name'].startswith('b') and not s['slide_name'].startswith('f'), slides_info)
-
     slides_info = list(train_slides) if mode == 'train' else list(eval_slides)
-
     slide_num = 0
+    data = np.array([]).reshape((0, 224, 224, 3))
+
+    labels = []
+
+    num_tiles_prefetch = 3
 
     while True:
-        data = np.array([]).reshape((0, 224, 224, 3))
-        labels = []
-        index = 0
-        while index < 3:
+        tiles = []
+        for _ in range(num_tiles_prefetch):
             current_slide = slides_info[slide_num]
             slide = utils.open_wsi(current_slide['slide_path'])
             zoom = DeepZoomGenerator(slide, tile_size=224, overlap=0)
@@ -86,31 +101,50 @@ def feed_slides_generator(normal_slides_info, tumor_slides_info, normal_selected
             # Find the deep zoom level corresponding to the requested magnification
             dzg_level_x = utils.get_x_zoom_level(current_slide['highest_zoom_level'], current_slide['slide_magnification'],
                                                  10)
-
             selected_tiles_dir = normal_selected_tiles_dir if current_slide['label'] == 0 else tumor_selected_tiles_dir
-            tiles = []
             print(">> Getting tiles..")
             slide_tiles_coords = np.load(os.path.join(selected_tiles_dir, current_slide['slide_name'] + '.npy'))
+            labels = [*labels, *(current_slide["label"] for s in range(len(tiles)))]
+
             for coord in slide_tiles_coords:
-                # print(coord)
                 tile = zoom.get_tile(dzg_level_x, (coord[0], coord[1]))
                 np_tile = utils.normalize_staining(tile)
-                # print(np_tile.shape)
                 tiles.append(np_tile)
 
-            print(">> tiles loaded")
-            print("Num tiles = %d" % len(tiles))
+        while len(data) > batch_size:
+            yield np.array(data[batch_size:]), np.array(labels[batch_size:]).reshape(-1, 1)
+            del data[batch_size:]
+            del labels[batch_size:]
 
-            data = np.concatenate((data, tiles))
-            #label = "normal" if current_slide["label"] == 0 else "tumor"
-            labels = [*labels, *(current_slide["label"] for s in range(len(tiles)))]
-            slide_num += 1
-            index += 1
-            if slide_num == len(slides_info):
-                slide_num = 0
+        slide_num += num_tiles_prefetch
+        if slide_num > len(slides_info):
+            slide_num = 0
 
-        print("Shape: ", data.shape)
-        yield np.array(data), np.array(labels).reshape(-1, 1)
+
+def get_train_ds_len(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir, tumor_selected_tiles_dir):
+    slides_info = [*normal_slides_info, *tumor_slides_info]
+    train_slides = list(filter(lambda s: s['slide_name'].startswith('b') or s['slide_name'].startswith('f'), slides_info))
+
+    acc = 0
+    for slide in train_slides:
+        selected_tiles_dir = normal_selected_tiles_dir if slide['label'] == 0 else tumor_selected_tiles_dir
+        slide_tiles_coords = np.load(os.path.join(selected_tiles_dir, slide['slide_name'] + '.npy'))
+        acc += len(slide_tiles_coords)
+
+    return acc
+
+
+def get_eval_ds_len(normal_slides_info, tumor_slides_info, normal_selected_tiles_dir, tumor_selected_tiles_dir):
+    slides_info = [*normal_slides_info, *tumor_slides_info]
+    eval_slides = filter(lambda s: not s['slide_name'].startswith('b') and not s['slide_name'].startswith('f'), slides_info)
+
+    acc = 0
+    for slide in eval_slides:
+        selected_tiles_dir = normal_selected_tiles_dir if slide['label'] == 0 else tumor_selected_tiles_dir
+        slide_tiles_coords = np.load(os.path.join(selected_tiles_dir, slide['slide_name'] + '.npy'))
+        acc += len(slide_tiles_coords)
+
+    return acc
 
 
 def plot_training(H, N):

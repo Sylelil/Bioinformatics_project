847f5f2a8370eaf4508935f2adc4764f82b1bc8f
=Cristian Traina <crissstian96@gmail.com>
2021-04-13 23:28:56 +0200

    avanzamento fine tuning

--- config/images/config.py
+++ config/images/config.py
@@ -4,3 +4,13 @@ from pathlib import Path
 BASE_DIR = Path('.')
 selected_tiles_dir = BASE_DIR / 'results' / 'images' / 'selected_tiles' / 'coords'
 
+
+# Labels
+NORMAL_LABEL = '0'
+TUMOR_LABEL = '1'
+
+# Data augmentation
+MULTIPLIER = 5
+
+# Ottimizzazioni
+NUM_SLIDES_PREFETCH = 3
--- src/images/features_extraction_methods/fine_tuning.py
+++ src/images/features_extraction_methods/fine_tuning.py
@@ -31,7 +31,7 @@ def fine_tuning(train_slides_info, test_slides_info, y_train, y_test):
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
 
-    batch_size = 100
+    batch_size = 32
     train_gen = feed_slides_generator(train_slides_info, y_train, batch_size, mode="train")
     eval_gen = feed_slides_generator(test_slides_info, y_test, batch_size, mode="eval")
     test_gen = feed_slides_generator(test_slides_info, y_test, batch_size, mode="eval")
@@ -53,7 +53,7 @@ def fine_tuning(train_slides_info, test_slides_info, y_train, y_test):
         layer.trainable = False
 
     print("[INFO] compiling model...")
-    opt = SGD(lr=1e-4, momentum=0.9)
+    opt = SGD(learning_rate=1e-4, momentum=0.9)
     model.compile(loss="binary_crossentropy", optimizer=opt,
                   metrics=["accuracy"])
 
@@ -63,7 +63,9 @@ def fine_tuning(train_slides_info, test_slides_info, y_train, y_test):
         steps_per_epoch=train_len // batch_size,
         validation_data=eval_gen,
         validation_steps=val_len // batch_size,
-        epochs=10)
+        epochs=10,
+        shuffle=True
+    )
 
     model.save("fine_tuned")
 
@@ -78,11 +80,11 @@ def feed_slides_generator(slides_info, labels_info, batch_size, mode='train'):
     slide_num = 0
     data = np.array([]).reshape((0, 224, 224, 3))
     labels = []
-    num_tiles_prefetch = 3
+    num_slides_prefetch = cfg.NUM_SLIDES_PREFETCH
 
     while True:
         tiles = []
-        for i in range(num_tiles_prefetch):
+        for i in range(num_slides_prefetch):
             current_slide = slides_info[slide_num+i]
             current_label = labels_info[slide_num+i]
 
@@ -100,31 +102,44 @@ def feed_slides_generator(slides_info, labels_info, batch_size, mode='train'):
                 tile = zoom.get_tile(dzg_level_x, (coord[0], coord[1]))
                 np_tile = utils.normalize_staining(tile)
                 tiles.append(np_tile)
-                if mode == 'train' and current_label == '1':
-                    data_augmentation = tf.keras.Sequential([
-                        layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
-                        layers.experimental.preprocessing.RandomRotation(0.2),
-                    ])
-                    augmented_tile = data_augmentation(np.expand_dims(np_tile, axis=0))
-                    tiles.append(np.squeeze(augmented_tile.numpy(), axis=0))
+                if mode == 'train' and current_label == cfg.NORMAL_LABEL:
+                    for _ in range(cfg.MULTIPLIER):
+                        # Facciamo attenzione a non introdurre alterazioni del colore
+                        # (saturazione, contrasto, ecc) perché, essendo sbilanciati, rischierebbero
+                        # di specializzare la rete neurale su queste alterazioni
+                        data_augmentation = tf.keras.Sequential([
+                            layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+                            layers.experimental.preprocessing.RandomRotation(0.2),
+                            layers.experimental.preprocessing.RandomZoom(0.4),
+                        ])
+                        augmented_tile = data_augmentation(np.expand_dims(np_tile, axis=0))
+                        tiles.append(np.squeeze(augmented_tile.numpy(), axis=0))
 
             data = np.concatenate((data, tiles))
 
             labels = [*labels, *(float(current_label) for s in range(len(tiles)))]
-            tiles = []
             print("Extracted ", len(slide_tiles_coords), " tiles")
+            print("Now data is ", len(data), " and labels is ", len(labels))
+            tiles = []
 
-        print("Extracted ", num_tiles_prefetch, " slides")
+        print("Extracted ", num_slides_prefetch, " slides")
         print("The data len is ", len(data), " while batch_size is ", batch_size)
+
+        # shuffling
+        idx = np.random.permutation(len(data))
+        data = data[idx]
+        labels = np.array(labels)[idx]
+
+        # batching
         while len(data) > batch_size:
             print("Yielding ", len(np.array(data[:batch_size])), len(np.array(labels[:batch_size]).reshape(-1, 1)))
             yield np.array(data[:batch_size]), np.array(labels[:batch_size]).reshape(-1, 1)
             data = np.delete(data, np.s_[:batch_size], 0)
             print("data is now ", len(data))
-            del labels[:batch_size]
+            labels = np.delete(labels, np.s_[:batch_size], 0)
             print("labels is now ", len(labels))
 
-        slide_num += num_tiles_prefetch
+        slide_num += num_slides_prefetch
         if slide_num > len(slides_info):
             slide_num = 0
 
@@ -134,7 +149,9 @@ def get_ds_len(slides_info):
     for slide in slides_info:
         coords_path = os.path.join(cfg.selected_tiles_dir, slide['slide_name'] + '.npy')
         slide_tiles_coords = np.load(coords_path)
-        acc += len(slide_tiles_coords)
+        multiplier = cfg.MULTIPLIER if int(slide["label"]) == cfg.NORMAL_LABEL else 1  # Moltiplichiamo il numero di tile per il moltiplicatore per correggere la classe sbilanciata
+        print("label is ", "normal" if int(slide["label"]) == cfg.NORMAL_LABEL else "tumor", "; then multiplier is ", multiplier)
+        acc += multiplier * len(slide_tiles_coords)
 
     return acc
 
@@ -157,6 +174,8 @@ def plot_training(H, N):
 # 2: capire come estrarre le tile, feedarle
 # 3: froze / unfroze
 # fare data augmentation
+# unfroze di più livelli
+# ottimizzare con asincrono / multithread
 # cambiare metrica (matthew...): va in conflitto con SMOTE?
 # 4: rileggere il capitolo prediction, e se facessimo il feed delle tile, poi facciamo la prediction di un'immagine dividendola in tile e si inferisce la soglia
 # poi possiamo utilizzare lo stesso procedimento per etichettare le singole tile

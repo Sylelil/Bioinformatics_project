aggiunto creazione report e plots di classificazione finale, spostato parametro n_components negli argomenti dello script, fix in plot explained variance

src/integration/classification_methods/nn_classification.py
@@ -4,12 +4,39 @@ import pandas as pd
 from pathlib import Path
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+
+from config import paths
 from src.integration import data_generator, plots, utils
 from src.integration.classification_methods import common
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
+
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+
+def generate_classification_results(args, params, y_test, y_pred_test, y_train, y_pred_train, test_scores):
+    # path to save results:
+    experiment_descr = f"CLF_{args.classification_method}_PCA_{params['pca']['n_components']}_BAL_{args.balancing}"
+    results_path = Path(paths.integration_classification_results_dir) / experiment_descr
+    if not os.path.exists(results_path):
+        os.makedirs(results_path)
+
+    # convert predicted probabilities (output of sigmoid) to 0/1 labels:
+    y_pred_test_labels = [1 if x > 0.5 else 0 for x in y_pred_test]
+    y_pred_train_labels = [1 if x > 0.5 else 0 for x in y_pred_train]
+
+    # generate classification report:
+    experiment_info = {}
+    experiment_info['Classification method'] = str(args.classification_method)
+    experiment_info['PCA n. components'] = str(params['pca']['n_components'])
+    experiment_info['Class balancing method'] = str(args.balancing)
+    utils.generate_classification_report(results_path, y_test, y_pred_test_labels, test_scores, experiment_info)
+
+    # generate plots:
+    utils.generate_classification_plots(results_path, y_test, y_pred_test_labels, y_train, y_pred_train_labels)
+
+    print('>> Done')
 
 
 def make_model(n_input_features, units_1, units_2, metrics=common.METRICS_keras, output_bias=None):
@@ -51,7 +78,8 @@ def mpl_classify(X_train, y_train, X_val, y_val, X_test, y_test, mlp_settings, u
     """
 
     print(">> Creating MultiLayer Perceptron model...")
-    model = make_model(mlp_settings['n_input_features'], units_1=mlp_settings['units_1'], units_2=mlp_settings['units_2'])
+    model = make_model(mlp_settings['n_input_features'], units_1=mlp_settings['units_1'],
+                       units_2=mlp_settings['units_2'])
     model.summary()
 
     print(">> Fitting model on train data...")
@@ -62,32 +90,19 @@ def mpl_classify(X_train, y_train, X_val, y_val, X_test, y_test, mlp_settings, u
                         callbacks=mlp_settings['early_stopping'],
                         validation_data=(X_val, (None if use_generators else y_val)),
                         class_weight=mlp_settings['class_weight'],
-                        verbose=1)
+                        verbose=2)
 
     plots.plot_train_val_results(history)
 
-    train_predictions_baseline = model.predict(X_train, batch_size=mlp_settings['BATCH_SIZE'])
-    test_predictions_baseline = model.predict(X_test, batch_size=mlp_settings['BATCH_SIZE'])
+    y_pred_train = model.predict(X_train, batch_size=mlp_settings['BATCH_SIZE'])
+    y_pred_test = model.predict(X_test, batch_size=mlp_settings['BATCH_SIZE'])
 
     print('Evaluating model on the test dataset...')
-    baseline_results = model.evaluate(X_test, (None if use_generators else y_test), batch_size=mlp_settings['BATCH_SIZE'], verbose=0)
+    test_scores_list = model.evaluate(X_test, (None if use_generators else y_test), batch_size=mlp_settings['BATCH_SIZE'],
+                                 verbose=0)
+    test_scores = dict(zip(model.metrics_names, test_scores_list))
 
-    for name, value in zip(model.metrics_names, baseline_results):
-        print(name, ': ', value)
-    print()
-
-    plots.plot_test_results(y_test, test_predictions_baseline, y_train, train_predictions_baseline)
-
-    plt.show()
-
-
-    # compute patch score and patient score:
-    print('>> Computing patch score...')
-    patch_score = common.compute_patch_score(y_test, train_predictions_baseline)
-    print(f'patch_score = {patch_score}')
-    print('>> Computing patient score...')
-    patient_avg_score, patent_stddev_score = common.compute_patient_score(train_predictions_baseline)
-    print(f'patient_score = {patient_avg_score} +- {patent_stddev_score}')
+    return y_pred_test, y_pred_train, test_scores
 
 
 def pca_nn_classifier(args, params, train_filepath, val_filepath, test_filepath):
@@ -99,7 +114,8 @@ def pca_nn_classifier(args, params, train_filepath, val_filepath, test_filepath)
        :param val_filepath: validation data path.
        :param test_filepath: test data path.
     """
-    X_train, y_train, X_val, y_val, X_test, y_test = utils.compute_scaling_pca(params, train_filepath, val_filepath, test_filepath)
+    X_train, y_train, X_val, y_val, X_test, y_test = utils.compute_scaling_pca(params, train_filepath, val_filepath,
+                                                                               test_filepath)
 
     class_weight = None
     if args.balancing and args.balancing != 'weights':
@@ -110,20 +126,23 @@ def pca_nn_classifier(args, params, train_filepath, val_filepath, test_filepath)
         class_weight = common.compute_class_weights(y_train)
 
     mlp_settings = {
-        'n_input_features' : params['pca']['n_components'],
-        'EPOCHS' : params['nn']['epochs'],
-        'BATCH_SIZE' : params['nn']['batchsize'],
-        'early_stopping' : tf.keras.callbacks.EarlyStopping(monitor='val_prc',
-                                                            verbose=1,
-                                                            patience=10,
-                                                            mode='max',
-                                                            restore_best_weights=True),
-        'class_weight' : class_weight,
-        'units_1' : 32,
+        'n_input_features': params['pca']['n_components'],
+        'EPOCHS': params['nn']['epochs'],
+        'BATCH_SIZE': params['nn']['batchsize'],
+        'early_stopping': tf.keras.callbacks.EarlyStopping(monitor='val_prc',
+                                                           verbose=1,
+                                                           patience=10,
+                                                           mode='max',
+                                                           restore_best_weights=True),
+        'class_weight': class_weight,
+        'units_1': 32,
         'units_2': 16,
     }
 
-    mpl_classify(X_train, y_train, X_val, y_val, X_test, y_test, mlp_settings)
+    y_pred_test, y_pred_train, test_scores = mpl_classify(X_train, y_train, X_val, y_val, X_test, y_test,
+                                                               mlp_settings)
+
+    generate_classification_results(args, params, y_test, y_pred_test, y_train, y_pred_train, test_scores)
 
 
 def nn_classifier(args, params, train_filepath, val_filepath, test_filepath):
@@ -170,7 +189,7 @@ def nn_classifier(args, params, train_filepath, val_filepath, test_filepath):
     with open(train_filepath, "r") as f:
         line = f.readline()
         line = line.strip().split(",")
-        n_features = len(line) -1  # -1 because we don't want to consider label!
+        n_features = len(line) - 1  # -1 because we don't want to consider label!
 
     balancer = None
     class_weight = None
@@ -186,31 +205,32 @@ def nn_classifier(args, params, train_filepath, val_filepath, test_filepath):
                                                         balancer=balancer)
     print(f">> Creating validation data generator with scaler...")
     val_generator = data_generator.csv_data_generator(val_filepath,
-                                                        batchsize=batchsize,
-                                                        scaler=scaler,
-                                                        balancer=None)
+                                                      batchsize=batchsize,
+                                                      scaler=scaler,
+                                                      balancer=None)
     print(f">> Creating test data generator with scaler...")
     test_generator = data_generator.csv_data_generator(test_filepath,
-                                                        batchsize=batchsize,
-                                                        scaler=scaler,
-                                                        balancer=None)
+                                                       batchsize=batchsize,
+                                                       scaler=scaler,
+                                                       balancer=None)
     mlp_settings = {
-        'n_input_features' : n_features,
-        'EPOCHS' : params['nn']['epochs'],
-        'BATCH_SIZE' : batchsize,
-        'early_stopping' : tf.keras.callbacks.EarlyStopping(monitor='val_prc',
-                                                            verbose=1,
-                                                            patience=10,
-                                                            mode='max',
-                                                            restore_best_weights=True),
-        'class_weight' : class_weight,
-        'units_1' : 2048,
+        'n_input_features': n_features,
+        'EPOCHS': params['nn']['epochs'],
+        'BATCH_SIZE': batchsize,
+        'early_stopping': tf.keras.callbacks.EarlyStopping(monitor='val_prc',
+                                                           verbose=1,
+                                                           patience=10,
+                                                           mode='max',
+                                                           restore_best_weights=True),
+        'class_weight': class_weight,
+        'units_1': 2048,
         'units_2': 1024,
     }
 
-    mpl_classify(X_train=train_generator, y_train=y_train,
-                 X_val=val_generator, y_val=y_val,
-                 X_test=test_generator, y_test=y_test,
-                 mlp_settings=mlp_settings,
-                 use_generators=True)
+    y_pred_test, y_pred_train, test_scores = mpl_classify(X_train=train_generator, y_train=y_train,
+                                                               X_val=val_generator, y_val=y_val,
+                                                               X_test=test_generator, y_test=y_test,
+                                                               mlp_settings=mlp_settings,
+                                                               use_generators=True)
 
+    generate_classification_results(args, params, y_test, y_pred_test, y_train, y_pred_train, test_scores)

src/integration/classification_methods/shallow_classification.py
@@ -1,10 +1,14 @@
+import os
 from pathlib import Path
 
+from pandas import DataFrame
 from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import plot_confusion_matrix, plot_roc_curve
 from sklearn.model_selection import KFold
 from sklearn.svm import LinearSVC
 from tqdm import tqdm
 
+from config import paths
 from src.integration import utils, plots
 from src.integration.classification_methods import common
 from sklearn import metrics
@@ -13,19 +17,18 @@ import pandas as pd
 import numpy as np
 from src.data_manipulation import concatenate_features
 
-colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-
 
 def get_classifier(hyperparam, method_name, balancing, random_state):
-    if method_name == 'svc':
+    if method_name == 'linearsvc':
         classifier = LinearSVC(C=hyperparam,
                                class_weight=('balanced' if balancing == 'weights' else None),
                                random_state=random_state)
     else:
         classifier = SGDClassifier(alpha=hyperparam,
-                                   max_iter=15,  # np.ceil(10**6 / n_samples)
+                                   max_iter=20,  # np.ceil(10**6 / n_samples)
                                    class_weight=('balanced' if balancing == 'weights' else None),
-                                   random_state=random_state)
+                                   random_state=random_state,
+                                   average=True) # Averaged SGD works best with a larger number of features and a higher eta0
     return classifier
 
 
@@ -49,7 +52,7 @@ def shallow_classifier(args, params, train_filepath, val_filepath, test_filepath
     else:
         metric = metrics.matthews_corrcoef
 
-    if args.method == 'svc':
+    if args.classification_method == 'linearsvc':
         print(">> Finding best hyperparameter C for LinearSVC...")
         grid = [0.0001, 0.001, 0.01, 0.1, 1] # C
     else:
@@ -59,9 +62,9 @@ def shallow_classifier(args, params, train_filepath, val_filepath, test_filepath
     best_score = -1
     best_hyperparam = None
     for hyperparam in grid:
-        print(f"{'C' if args.method == 'svc' else 'alpha'}={hyperparam}:")
+        print(f"{'C' if args.classification_method == 'linearsvc' else 'alpha'}={hyperparam}:")
         classifier = get_classifier(hyperparam=hyperparam,
-                                    method_name=args.method,
+                                    method_name=args.classification_method,
                                     balancing=args.balancing,
                                     random_state=params['general']['random_state'])
         classifier.fit(X_train, y_train)
@@ -73,39 +76,42 @@ def shallow_classifier(args, params, train_filepath, val_filepath, test_filepath
             best_hyperparam = hyperparam
 
     # evaluate the performance of the model on test
+    print()
     print(
-        f"Best {metric.__name__} ({'C' if args.method == 'svc' else 'alpha'}={best_hyperparam}): {best_score}")
-
-    print(f">> Training with best {'LinearSVC' if args.method == 'svc' else 'SGDClassifier'} model...")
+        f"Best {metric.__name__} ({'C' if args.classification_method == 'linearsvc' else 'alpha'}={best_hyperparam}): {best_score}")
+    print()
+    print(f">> Training with best {'LinearSVC' if args.classification_method == 'linearsvc' else 'SGDClassifier'} model...")
     best_classifier = get_classifier(hyperparam=best_hyperparam,
-                                     method_name=args.method,
+                                     method_name=args.classification_method,
                                      balancing=args.balancing,
                                      random_state=params['general']['random_state'])
 
     best_classifier.fit(X_train, y_train)
     print(">> Testing...")
+    print()
     y_pred_test = best_classifier.predict(X_test)
     y_pred_train = best_classifier.predict(X_train)
 
-    print('Test scores:')
-    test_scores = []
+    test_scores = {}
     for metric in common.METRICS_skl:
-        test_scores.append((metric.__name__, metric(y_test, y_pred_test)))
-    for name, value in test_scores:
-        print(name, ': ', value)
-    print()
+        test_scores[metric.__name__] = metric(y_test, y_pred_test)
 
-    print(metrics.classification_report(y_test, y_pred_test))
+    # path to save results:
+    experiment_descr = f"CLF_{args.classification_method}_PCA_{params['pca']['n_components']}_BAL_{args.balancing}"
+    results_path = Path(paths.integration_classification_results_dir) / experiment_descr
+    if not os.path.exists(results_path):
+        os.makedirs(results_path)
 
-    plots.plot_test_results(y_test, y_pred_test, y_train, y_pred_train)
-    plt.show()
+    # generate classification report:
+    experiment_info = {}
+    experiment_info['Classification method'] = str(args.classification_method)
+    experiment_info['PCA n. components'] = str(params['pca']['n_components'])
+    experiment_info['Class balancing method'] = str(args.balancing)
+    experiment_info['Best hyperparameter'] = f"{'C' if args.classification_method == 'linearsvc' else 'alpha'}={best_hyperparam}"
+    experiment_info['Best validation score'] = f"{metric.__name__}={best_score}"
+    utils.generate_classification_report(results_path, y_test, y_pred_test, test_scores, experiment_info)
 
-    # compute patch score and patient score:
-    print('>> Computing patch score...')
-    patch_score = common.compute_patch_score(y_test, y_pred_test)
-    print(f'patch_score = {patch_score}')
-    print('>> Computing patient score...')
-    patient_avg_score, patent_stddev_score = common.compute_patient_score(y_pred_test)
-    print(f'patient_score = {patient_avg_score} +- {patent_stddev_score}')
-
+    # generate plots:
+    #utils.generate_classification_plots(results_path, best_classifier, X_train, y_train, X_test, y_test)
+    utils.generate_classification_plots(results_path, y_test, y_pred_test, y_train, y_pred_train)
     print('>> Done')

src/integration/plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib as mpl
-from sklearn.metrics import confusion_matrix, precision_recall_curve, roc_curve
+from sklearn import metrics
 import numpy as np
 
 mpl.rcParams['figure.figsize'] = (12, 10)
@@ -61,32 +61,26 @@ def plot_metrics(history):
         if metric == 'loss':
             plt.ylim([0, plt.ylim()[1]])
         elif metric == 'auc':
-            plt.ylim([0.8,1])
+            plt.ylim([0.8, 1])
         else:
-            plt.ylim([0,1])
+            plt.ylim([0, 1])
 
         plt.legend()
 
 
-def plot_cm(labels, predictions, p=0.5):
+def plot_cm(labels, predictions):
     """
        Description: Plot Confusion Matrix.
        :param labels: ground truth labels.
        :param predictions: predicted labels.
        :param p: threshold.
     """
-    cm = confusion_matrix(labels, predictions > p)
-    plt.figure(figsize=(5,5))
+    cm = metrics.confusion_matrix(labels, predictions)
+    plt.figure(figsize=(5, 5))
     sns.heatmap(cm, annot=True, fmt="d")
-    plt.title('Confusion matrix @{:.2f}'.format(p))
-    plt.ylabel('Actual label')
+    plt.title('Confusion matrix')
+    plt.ylabel('True label')
     plt.xlabel('Predicted label')
-
-    print('Normal Detected (True Negatives): ', cm[0][0])
-    print('Normal Incorrectly Detected (False Positives): ', cm[0][1])
-    print('Tumor Missed (False Negatives): ', cm[1][0])
-    print('Tumor Detected (True Positives): ', cm[1][1])
-    print('Total Tumor: ', np.sum(cm[1]))
 
 
 def plot_roc(name, labels, predictions, **kwargs):
@@ -97,16 +91,18 @@ def plot_roc(name, labels, predictions, **kwargs):
        :param predictions: predicted labels.
        :param **kwargs: plot arguments.
     """
-    fp, tp, _ = roc_curve(labels, predictions)
+    fp, tp, _ = metrics.roc_curve(labels, predictions)
+    auc = metrics.roc_auc_score(labels, predictions)
 
-    plt.plot(100*fp, 100*tp, label=name, linewidth=2, **kwargs)
-    plt.xlabel('False positives [%]')
-    plt.ylabel('True positives [%]')
-    plt.xlim([-0.5,20])
-    plt.ylim([80,100.5])
+    plt.plot(fp, tp, label=f'{name}, auc={auc}', linewidth=2, **kwargs)
+    plt.xlabel('False positive rate (Positive label: 1)')
+    plt.ylabel('True positive rate (Positive label: 1)')
+    plt.xlim([-0.1, 1.1])
+    plt.ylim([-0.1, 1.1])
     plt.grid(True)
     ax = plt.gca()
     ax.set_aspect('equal')
+    plt.legend(loc=0)
 
 
 def plot_prc(name, labels, predictions, **kwargs):
@@ -117,11 +113,11 @@ def plot_prc(name, labels, predictions, **kwargs):
        :param predictions: predicted labels.
        :param **kwargs: plot arguments.
     """
-    precision, recall, _ = precision_recall_curve(labels, predictions)
+    precision, recall, _ = metrics.precision_recall_curve(labels, predictions)
 
     plt.plot(precision, recall, label=name, linewidth=2, **kwargs)
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
+    plt.xlabel('Recall (Positive label: 1)')
+    plt.ylabel('Precision (Positive label: 1)')
     plt.xlim([-0.1, 1.1])
     plt.ylim([-0.1, 1.1])
     plt.grid(True)
@@ -135,31 +131,4 @@ def plot_train_val_results(history):
     plot_metrics(history)
     plt.figure()
 
-
-def plot_test_results(y_test, test_predictions_baseline, y_train, train_predictions_baseline):
-    plot_cm(y_test, test_predictions_baseline)
-    plt.figure()
-
-    plot_roc("Train Baseline", y_train, train_predictions_baseline, color=colors[0])
-    plot_roc("Test Baseline", y_test, test_predictions_baseline, color=colors[0], linestyle='--')
-    plt.legend(loc='lower right')
-    plt.figure()
-
-    plot_prc("Train Baseline", y_train, train_predictions_baseline, color=colors[0])
-    plot_prc("Test Baseline", y_test, test_predictions_baseline, color=colors[0], linestyle='--')
-    plt.legend(loc='lower right')
-    plt.figure()
-
-
-def plot_test_results_aggregated(y_test, test_predictions_baseline):
-    plot_cm(y_test, test_predictions_baseline)
-    plt.figure()
-
-    plot_roc("Test Baseline", y_test, test_predictions_baseline, color=colors[0], linestyle='--')
-    plt.legend(loc='lower right')
-    plt.figure()
-
-    plot_prc("Test Baseline", y_test, test_predictions_baseline, color=colors[0], linestyle='--')
-    plt.legend(loc='lower right')
-    plt.figure()
 

src/integration/utils.py
@@ -1,5 +1,9 @@
 import configparser
 import os
+import sys
+
+from pandas import DataFrame
+from sklearn import metrics
 from tqdm import tqdm
 import pandas as pd
 from pathlib import Path
@@ -8,8 +12,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import IncrementalPCA
 import matplotlib.pyplot as plt
 
+from config import paths
+from src.integration import plots
+from src.integration.classification_methods import common
 
-def __scaling_pca(params, concatenated_pca_path, train_filepath, val_filepath, test_filepath, get_explained_variance_ratio=False):
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+
+def __scaling_pca(params, concatenated_pca_path, train_filepath, val_filepath, test_filepath,
+                  get_explained_variance_ratio=False):
     x_train_pca_path = Path(concatenated_pca_path) / 'x_train.npy'
     y_train_pca_path = Path(concatenated_pca_path) / 'y_train.npy'
     x_val_pca_path = Path(concatenated_pca_path) / 'x_val.npy'
@@ -32,7 +43,8 @@ def __scaling_pca(params, concatenated_pca_path, train_filepath, val_filepath, t
     y_val = []
     X_test = []
     y_test = []
-    print(f">> Transforming train data with scaler and fitting incremental pca ({params['pca']['n_components']} components)...")
+    print(
+        f">> Transforming train data with scaler and fitting incremental pca ({params['pca']['n_components']} components)...")
     for chunk in tqdm(pd.read_csv(train_filepath, chunksize=batchsize, iterator=True, dtype='float64')):
         X_train_chunk = chunk.iloc[:, :-1]
         X_train_chunk_scaled = scaler.transform(X_train_chunk)
@@ -92,7 +104,7 @@ def compute_scaling_pca(params, train_filepath, val_filepath, test_filepath):
        :param test_filepath: path of test data.
        :returns: X_train, y_train, X_val, y_val, X_test, y_test: data and labels
     """
-    concatenated_pca_path = Path('assets') / f"concatenated_pca_{params['pca']['n_components']}"
+    concatenated_pca_path = paths.concatenated_pca_dir / f"concatenated_pca_{params['pca']['n_components']}"
 
     if os.path.exists(concatenated_pca_path):
         print('>> Reading files with scaled and pca data previously computed...')
@@ -104,24 +116,136 @@ def compute_scaling_pca(params, train_filepath, val_filepath, test_filepath):
         y_test = np.load(Path(concatenated_pca_path) / 'y_test.npy')
     else:
         os.mkdir(concatenated_pca_path)
-        X_train, y_train, X_val, y_val, X_test, y_test = __scaling_pca(params, concatenated_pca_path, train_filepath, val_filepath, test_filepath)
+        X_train, y_train, X_val, y_val, X_test, y_test = __scaling_pca(params, concatenated_pca_path, train_filepath,
+                                                                       val_filepath, test_filepath)
 
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 
-def plot_cumulative_explained_variance_pca(params, train_filepath, val_filepath, test_filepath):
-    print(">> Starting procedure to plot cumulative explained variance of PCA components...")
-    concatenated_pca_path = Path('assets') / f"concatenated_pca_{params['pca']['n_components']}"
+def plot_explained_variance_pca(params, train_filepath, val_filepath, test_filepath):
+    print(
+        f">> Starting procedure to plot explained variance of Principal Components (up to {params['pca']['n_components']} components)...")
+    concatenated_pca_path = paths.concatenated_pca_dir / f"concatenated_pca_{params['pca']['n_components']}"
     if not os.path.exists(concatenated_pca_path):
-        os.mkdir(concatenated_pca_path)
-    explained_variance_ratio = __scaling_pca(params, concatenated_pca_path, train_filepath, val_filepath, test_filepath,
-                                             get_explained_variance_ratio=True)
-    print(">> Plotting cumulative explained variance...")
-    plt.plot(np.cumsum(explained_variance_ratio))
-    plt.xlabel('number of components')
-    plt.ylabel('cumulative explained variance')
+        os.makedirs(concatenated_pca_path)
+    explained_variance_ratio = __scaling_pca(params, concatenated_pca_path, train_filepath,
+                                                                 val_filepath, test_filepath,
+                                                                 get_explained_variance_ratio=True)
+    print(">> Plotting individual and cumulative explained variance...")
+    plt.figure()
+    plt.plot(np.cumsum(explained_variance_ratio), label='Cumulative explained variance', linewidth=2, marker='.')
+    plt.plot(explained_variance_ratio, label='Individual explained variance', linewidth=2, marker='.')
+    plt.ylabel('Explained variance ratio')
+    plt.xlabel('Number of components')
+    plt.legend(loc='best')
+    plt.savefig(Path(concatenated_pca_path) / 'explained_variance')
     plt.show()
     print('>> Done.')
+
+
+def __classification_report(y_test, y_pred_test, test_scores):
+    report = {}
+
+    # compute confusion matrix values:
+    confusion_matrix = metrics.confusion_matrix(y_test, y_pred_test)
+    report['Normal Detected (True Negatives)'] = confusion_matrix[0][0]
+    report['Normal Incorrectly Detected (False Positives)'] = confusion_matrix[0][1]
+    report['Tumor Missed (False Negatives)'] = confusion_matrix[1][0]
+    report['Tumor Detected (True Positives)'] = confusion_matrix[1][1]
+    report['Total Tumor'] = np.sum(confusion_matrix[1])
+
+    # add test scores:
+    report.update(test_scores)
+
+    # compute patch score and patient score:
+    report['patch_score'] = common.compute_patch_score(y_test, y_pred_test)
+    report['patient_avg_score'], patent_stddev_score = common.compute_patient_score(y_pred_test)
+
+    # compute further per-class scores:
+    per_class_report_dict = metrics.classification_report(y_test, y_pred_test)
+
+    return report, per_class_report_dict
+
+
+def __print_classification_report(experiment_info, report, per_class_report, file=sys.stdout):
+    print('Experiment details:', file=file)
+    print('-------------------', file=file)
+    for k, v in experiment_info.items():
+        print("{:<50} {:<15}".format(k, v), file=file)
+    print(file=file)
+
+    print('Test results:', file=file)
+    print('-------------', file=file)
+    for k, v in report.items():
+        print("{:<50} {:<15}".format(k, v), file=file)
+    print(file=file)
+    print(per_class_report, file=file)
+
+
+def generate_classification_report(save_path, y_test, y_pred_test, test_scores, experiment_info):
+    # generate report:
+    print('>> Generating classification report...')
+    report, per_class_report = __classification_report(y_test, y_pred_test, test_scores)
+
+    # print on stdout:
+    __print_classification_report(experiment_info, report, per_class_report)
+
+    # print on file:
+    report_path = Path(save_path) / 'report.txt'
+    print(f'>> Saving classification report on file {report_path}...')
+    with open(report_path, 'w') as f:
+        __print_classification_report(experiment_info, report, per_class_report, file=f)
+
+'''
+def generate_classification_plots(save_path, classifier, X_train, y_train, X_test, y_test):
+    print(f'Generating classification plots, saved in {save_path}...')
+
+    # plot confusion matrix
+    plt.figure()
+    metrics.plot_confusion_matrix(classifier, X_test, y_test, values_format='')
+    plt.savefig(Path(save_path) / 'confusion_matrix')
+
+    # plot roc
+    plt.figure()
+    metrics.plot_roc_curve(classifier, X_train, y_train, ax=plt.gca(), name="Train")
+    metrics.plot_roc_curve(classifier, X_test, y_test, ax=plt.gca(), name="Test")
+    plt.legend(loc='lower right')
+    plt.savefig(Path(save_path) / 'roc')
+
+    # plot precision-recall curve
+    plt.figure()
+    metrics.plot_precision_recall_curve(classifier, X_train, y_train, ax=plt.gca(), name="Train")
+    metrics.plot_precision_recall_curve(classifier, X_test, y_test, ax=plt.gca(), name="Test")
+    plt.legend(loc='lower right')
+    plt.savefig(Path(save_path) / 'prc')
+
+    plt.show()
+'''
+
+def generate_classification_plots(save_path, y_test, y_pred_test, y_train, y_pred_train):
+    print(f'>> Generating classification plots and saving them in {save_path}...')
+
+    # plot confusion matrix
+    plt.figure()
+    plots.plot_cm(y_test, y_pred_test)
+    plt.savefig(Path(save_path) / 'confusion_matrix')
+
+    # plot roc
+    plt.figure()
+    plots.plot_roc("Train", y_train, y_pred_train)
+    plots.plot_roc("Test", y_test, y_pred_test)
+    plt.legend(loc='lower right')
+    plt.savefig(Path(save_path) / 'roc')
+
+    # plot precision-recall curve
+    plt.figure()
+    plots.plot_prc("Train", y_train, y_pred_train)
+    plots.plot_prc("Test", y_test, y_pred_test)
+    plt.legend(loc='lower right')
+    plt.savefig(Path(save_path) / 'prc')
+
+    plt.show()
+
 
 def read_config_file(config_file_path):
     """
@@ -141,12 +265,6 @@ def read_config_file(config_file_path):
     else:
         params['general']['random_state'] = config.getint('general', 'random_state')
 
-    # pca
-    params['pca'] = {}
-    params['pca']['percentage_of_variance'] = config.getfloat('pca', 'percentage_of_variance')
-    params['pca']['n_components'] = config.getint('pca', 'n_components')
-    params['pca']['plot_cumulative_explained_variance'] = config.getboolean('pca', 'plot_cumulative_explained_variance')
-
     # preprocessing
     params['preprocessing'] = {}
     params['preprocessing']['batchsize'] = config.getint('preprocessing', 'batchsize')
@@ -155,6 +273,10 @@ def read_config_file(config_file_path):
     params['nn'] = {}
     params['nn']['epochs'] = config.getint('nn', 'epochs')
     params['nn']['batchsize'] = config.getint('nn', 'batchsize')
+
+    # pca
+    params['pca'] = {}
+    params['pca']['n_components'] = None
 
     return params
 
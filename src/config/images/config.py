342854d1fe1a75e86ddf1104be2066dc1f129fae
Giorgia3 <s262606@studenti.polito.it>
2021-05-23 10:32:37 +0200

    fix threshold image config

--- src/config/images/config.py
+++ src/config/images/config.py
@@ -3,5 +3,6 @@ DESIRED_MAGNIFICATION = 10
 TILE_SIZE = 224
 SCALE_FACTOR = 32
 OVERLAP = 0
-
+THRESHOLD = 0.90   # Threshold parameter indicating the proportion of the tile area that should be foreground (tissue content)
+                   # in order to be selected. It should range between 0 and 1.
 USE_GPU = True
